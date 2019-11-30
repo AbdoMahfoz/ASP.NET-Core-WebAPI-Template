042670b28d095c208b19ddd500074ac89d614f21
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2019-11-30 03:34:35 +0200

    Clean-up

--- WebAPI/GenericControllerCreator/GenericController.cs
+++ WebAPI/GenericControllerCreator/GenericController.cs
@@ -12,7 +12,7 @@ namespace WebAPI.GenericControllerCreator
     [Route("api/[controller]")]
     [Produces("application/json")]
     [ApiController]
-    [GenericControllerNameAttribute]
+    [GenericControllerName]
     public class GenericController<T> : ControllerBase where T : BaseModel
     {
         private readonly IGenericLogic<T> _genericLogic;
@@ -26,9 +26,7 @@ namespace WebAPI.GenericControllerCreator
         ///     Returns all the ids of the instances of The Model.
         /// </summary>
         /// <response code="200">All Instances Ids</response>
-        /// <response code="401">UnAuthorized Access</response>
         [ProducesResponseType(200, Type = typeof(IEnumerable<int>))]
-        [ProducesResponseType(401, Type = typeof(string))]
         [HttpGet("GetAll")]
         public IActionResult GetAll()
         {
@@ -40,10 +38,6 @@ namespace WebAPI.GenericControllerCreator
         /// </summary>
         /// <param name="id">Instance Id</param>
         /// <response code="200">The Instance with the Id</response>
-        /// <response code="401">UnAuthorized Access</response>
-        [ProducesResponseType(403, Type = typeof(string))]
-        [ProducesResponseType(400, Type = typeof(string))]
-        [ProducesResponseType(401, Type = typeof(string))]
         [HttpGet("{id}")]
         public T Get(int id)
         {
@@ -60,12 +54,8 @@ namespace WebAPI.GenericControllerCreator
         /// <param name="instance">The instance</param>
         /// <response code="201">instance Inserted</response>
         /// <response code="400">Bad Request</response>
-        /// <response code="401">UnAuthorized Access</response>
-        /// <response code="403">Invalid Data</response>
         [ProducesResponseType(201, Type = typeof(int))]
         [ProducesResponseType(400, Type = typeof(string))]
-        [ProducesResponseType(401, Type = typeof(string))]
-        [ProducesResponseType(403, Type = typeof(string))]
         [HttpPost("Insert")]
         public IActionResult Insert([FromBody] T instance)
         {
@@ -83,19 +73,10 @@ namespace WebAPI.GenericControllerCreator
         /// </remarks>
         /// <param name="instances">List of instances</param>
         /// <response code="201">Entities Inserted</response>
-        /// <response code="400">Bad Request</response>
-        /// <response code="401">UnAuthorized Access</response>
-        /// <response code="403">Invalid Data</response>
         [ProducesResponseType(201, Type = typeof(IEnumerable<int>))]
-        [ProducesResponseType(400, Type = typeof(string))]
-        [ProducesResponseType(401, Type = typeof(string))]
-        [ProducesResponseType(403, Type = typeof(string))]
         [HttpPost("InsertRange")]
         public IActionResult InsertRange([FromBody] IEnumerable<T> instances)
         {
-            foreach (var enabler in instances)
-                if (enabler == null)
-                    return BadRequest();
             _genericLogic.InsertRange(instances).Wait();
             var ids = instances.Select(x => x.Id);
             return StatusCode(StatusCodes.Status201Created, ids);
@@ -109,13 +90,7 @@ namespace WebAPI.GenericControllerCreator
         /// </remarks>
         /// <param name="instance">The instance</param>
         /// <response code="202">Instance updated</response>
-        /// <response code="400">Bad Request</response>
-        /// <response code="401">UnAuthorized Access</response>
-        /// <response code="403">Invalid Data</response>
         [ProducesResponseType(202, Type = typeof(int))]
-        [ProducesResponseType(400, Type = typeof(string))]
-        [ProducesResponseType(401, Type = typeof(string))]
-        [ProducesResponseType(403, Type = typeof(string))]
         [HttpPut("Update")]
         public IActionResult Update([FromBody] T instance)
         {
@@ -132,13 +107,7 @@ namespace WebAPI.GenericControllerCreator
         /// </remarks>
         /// <param name="instances">The instance</param>
         /// <response code="202">Instances updated</response>
-        /// <response code="400">Bad Request</response>
-        /// <response code="401">UnAuthorized Access</response>
-        /// <response code="403">Invalid Data</response>
         [ProducesResponseType(202, Type = null)]
-        [ProducesResponseType(400, Type = typeof(string))]
-        [ProducesResponseType(401, Type = typeof(string))]
-        [ProducesResponseType(403, Type = typeof(string))]
         [HttpPut("UpdateRange")]
         public IActionResult UpdateRange([FromBody] IEnumerable<T> instances)
         {
@@ -157,13 +126,7 @@ namespace WebAPI.GenericControllerCreator
         /// </remarks>
         /// <param name="Id">The instance id</param>
         /// <response code="202">instance Deleted</response>
-        /// <response code="401">UnAuthorized Access</response>
-        /// <response code="403">Invalid Data</response>
-        /// <response code="403">instance Not Found</response>
         [ProducesResponseType(202, Type = null)]
-        [ProducesResponseType(401, Type = typeof(string))]
-        [ProducesResponseType(403, Type = typeof(string))]
-        [ProducesResponseType(404, Type = typeof(string))]
         [HttpDelete("Delete")]
         public IActionResult Delete(int Id)
         {
@@ -176,13 +139,7 @@ namespace WebAPI.GenericControllerCreator
         /// </summary>
         /// <param name="ids">The instances' ids</param>
         /// <response code="202">instances Deleted</response>
-        /// <response code="401">UnAuthorized Access</response>
-        /// <response code="403">Invalid Data</response>
-        /// <response code="403">Enablers Not Found</response>
         [ProducesResponseType(202, Type = null)]
-        [ProducesResponseType(401, Type = typeof(string))]
-        [ProducesResponseType(403, Type = typeof(string))]
-        [ProducesResponseType(404, Type = typeof(string))]
         [HttpDelete("DeleteRange")]
         public IActionResult DeleteEnablers([FromBody] IEnumerable<int> ids)
         {
